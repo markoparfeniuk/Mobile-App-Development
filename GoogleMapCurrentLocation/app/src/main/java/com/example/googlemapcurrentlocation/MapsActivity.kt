a48a0971d3097952e9fd2822f301c8273ee5bf8c
markoparfeniuk <marko.parfeniuk.pm.2020@lpnu.ua>
2022-11-06 15:32:47 +0200

    add recent changes

---
+++ GoogleMapCurrentLocation/app/src/main/java/com/example/googlemapcurrentlocation/MapsActivity.kt
@@ -0,0 +1,89 @@
+package com.example.googlemapcurrentlocation
+
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationProvider
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.googlemapcurrentlocation.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocationUser()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty()) {
+                getCurrentLocationUser()
+            }
+        }
+    }
+
+    private fun getCurrentLocationUser() {
+        if (ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+
+        val getLocation = fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+            location ->
+
+            if (location != null) {
+                currentLocation = location
+                Toast.makeText(applicationContext, currentLocation.latitude.toString() + "" +
+                currentLocation.longitude.toString(), Toast.LENGTH_LONG).show()
+
+                val mapFragment = supportFragmentManager
+                    .findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+            }
+        }
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+        val markerOptions = MarkerOptions().position(latLng).title("You are here")
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLng((latLng)))
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7f))
+        googleMap?.addMarker(markerOptions)
+    }
+}
